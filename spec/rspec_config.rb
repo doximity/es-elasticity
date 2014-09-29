1ca54adc7acdf468204a4cc305174869b14ebeae
Rodrigo Kochenburger <divoxx@gmail.com>
2014-09-29 09:59:48 -0700

    Log spec execution info

--- spec/rspec_config.rb
+++ spec/rspec_config.rb
@@ -7,16 +7,24 @@ def elastic_search_client
   @elastic_search_client = Elasticsearch::Client.new host: "http://0.0.0.0:9200"
 end
 
+logger = Logger.new("spec/log", File::WRONLY | File::APPEND | File::CREAT)
+
 RSpec.configure do |c|
   c.disable_monkey_patching!
 
+  c.before(:suite) do
+    logger.info "rspec.init Starting test suite execution"
+  end
+
   c.before(:each) do |example|
+    logger.info "rspec.spec #{example.full_description}"
+
     if example.metadata[:elasticsearch]
       client = elastic_search_client
     end
 
     Elasticity.configure do |e|
-      e.logger = Logger.new("spec/log", File::WRONLY | File::APPEND | File::CREAT)
+      e.logger = logger
       e.client = client
     end
   end
