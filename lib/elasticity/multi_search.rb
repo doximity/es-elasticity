8ec036ecbc8e73fc11bfef34dc03f4ca7aa32565
Emiliano Zilocchi <ezilocchi@doximity.com>
2017-06-02 13:39:42 -0300

    Make MultiSearch lazy load

--- lib/elasticity/multi_search.rb
+++ lib/elasticity/multi_search.rb
@@ -2,6 +2,7 @@ module Elasticity
   class MultiSearch
 
     def initialize(msearch_args = {})
+      @results  = {}
       @searches = {}
       @mappers  = {}
       @msearch_args = msearch_args
@@ -25,39 +26,36 @@ module Elasticity
     end
 
     def [](name)
-      results_collection[name]
+      # FIXME: How to initialize results
+      @results[name] ||= result_for(name)
     end
 
     private
 
-    def results_collection
-      @results_collection ||= fetch
+    def result_for(name)
+      index  = @searches.keys.index(name)
+      search = @searches[name]
+      resp   = response["responses"][index]
+
+      case
+      when search[:documents]
+        Search::Results.new(resp, search[:search_definition].body, search[:documents].method(:map_hit))
+      when search[:active_records]
+        Search::ActiveRecordProxy.map_response(search[:active_records], search[:search_definition].body, resp)
+      end
     end
 
-    def fetch
-      bodies = @searches.values.map do |hsh|
-        hsh[:search_definition].to_msearch_args
-      end
-
-      response = ActiveSupport::Notifications.instrument("multi_search.elasticity", args: { body: bodies }) do
+    def response
+      @response ||= ActiveSupport::Notifications.instrument("multi_search.elasticity", args: { body: bodies }) do
         args = { body: bodies.map(&:dup) }.reverse_merge(@msearch_args)
         Elasticity.config.client.msearch(args)
       end
-      results = {}
+    end
 
-      @searches.keys.each_with_index do |name, idx|
-        resp = response["responses"][idx]
-        search = @searches[name]
-
-        results[name] = case
-        when search[:documents]
-          Search::Results.new(resp, search[:search_definition].body, search[:documents].method(:map_hit))
-        when search[:active_records]
-          Search::ActiveRecordProxy.map_response(search[:active_records], search[:search_definition].body, resp)
-        end
+    def bodies
+      @bodies ||= @searches.values.map do |hsh|
+        hsh[:search_definition].to_msearch_args
       end
-
-      results
     end
   end
 end
